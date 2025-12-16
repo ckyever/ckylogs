7fa6397e0b1c9cb604c9718c6b7ef98f54054fc9
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-17 10:27:36 +1100

    Add login link to home page

--- frontend/public/src/App.jsx
+++ frontend/public/src/App.jsx
@@ -4,7 +4,6 @@ import { Outlet } from "react-router";
 function App() {
   return (
     <>
-      <h1>Ckylogs</h1>
       <Outlet />
     </>
   );
--- frontend/public/src/components/Home.jsx
+++ frontend/public/src/components/Home.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router";
+
 function Home() {
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <nav>
+        <ul>
+          <Link to="login">Login</Link>
+        </ul>
+      </nav>
     </div>
   );
 }
