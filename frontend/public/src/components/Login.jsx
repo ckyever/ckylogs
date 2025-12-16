c6ca7c9ec20753a16c09c1caedf531752be3eb56
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-17 10:46:48 +1100

    Add login form

--- frontend/public/src/components/Login.jsx
+++ frontend/public/src/components/Login.jsx
@@ -1,7 +1,24 @@
 function Login() {
   return (
     <div className="login">
-      <h2>Login Page</h2>
+      <h1>Login</h1>
+      <form>
+        <input
+          type="text"
+          name="username"
+          aria-label="username"
+          placeholder="Username"
+          required
+        ></input>
+        <input
+          type="password"
+          name="password"
+          aria-label="password"
+          placeholder="Password"
+          required
+        ></input>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
