4d0cc49f461cb8aa9828d100a656870b7a9fcb49
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-19 17:55:38 +1100

    Add initial post data to home page

--- frontend/public/src/components/Home.jsx
+++ frontend/public/src/components/Home.jsx
@@ -1,12 +1,30 @@
 import Logout from "./Logout.jsx";
+import { useEffect, useState } from "react";
 import { Link, useOutletContext } from "react-router";
 
 function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
   const { username } = useOutletContext();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/post`
+        );
+        const data = await response.json();
+        setPosts(data.posts);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
 
   return (
     <div className="home">
-      <h2>Home Page</h2>
+      <h1>Home Page</h1>
       {username && <div>Welcome {username}</div>}
       <nav>
         {username ? (
@@ -17,6 +35,21 @@ function Home() {
           </ul>
         )}
       </nav>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {posts.map((post) => {
+            return (
+              <li>
+                <h2>{post.title}</h2>
+                <span>{post.created_on}</span>
+                <p>{post.body}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
