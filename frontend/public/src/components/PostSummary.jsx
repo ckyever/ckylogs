ad8f848aaa60ea7cacf2e1024e087c295471606a
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-20 12:12:38 +1100

    Fix post link to only wrap the title and body to prevent hydration error

--- frontend/public/src/components/PostSummary.jsx
+++ frontend/public/src/components/PostSummary.jsx
@@ -2,12 +2,16 @@ import { Link } from "react-router";
 
 function PostSummary({ id, title, author, createdOn, body }) {
   return (
-    <Link to={`post/${id}`}>
-      <h2>{title}</h2>
+    <>
+      <Link to={`post/${id}`}>
+        <h2>{title}</h2>
+      </Link>
       <Link to={`user/${author}`}>{author}</Link>
       <div>{createdOn}</div>
-      <p>{body}</p>
-    </Link>
+      <Link to={`post/${id}`}>
+        <p>{body}</p>
+      </Link>
+    </>
   );
 }
 
