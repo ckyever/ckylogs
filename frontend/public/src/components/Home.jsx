0baa1b6f6623fe0501d200fff37b867ece73a0e8
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-20 14:26:53 +1100

    Move elements in home page navbar to new Navbar component

--- frontend/public/src/components/Home.jsx
+++ frontend/public/src/components/Home.jsx
@@ -1,26 +1,12 @@
-import Logout from "./Logout.jsx";
 import Navbar from "./Navbar.jsx";
 import PostList from "./PostList.jsx";
-import { Link, useOutletContext } from "react-router";
 
 function Home() {
-  const { username } = useOutletContext();
-
   return (
     <>
       <Navbar />
       <div className="home">
         <h2>Home Page</h2>
-        {username && <div>Welcome {username}</div>}
-        <nav>
-          {username ? (
-            <Logout></Logout>
-          ) : (
-            <ul>
-              <Link to="login">Login</Link>
-            </ul>
-          )}
-        </nav>
         <PostList endpoint="post" />
       </div>
     </>
--- frontend/public/src/components/Navbar.jsx
+++ frontend/public/src/components/Navbar.jsx
@@ -1,11 +1,22 @@
-import { Link } from "react-router";
+import Logout from "./Logout.jsx";
+import { Link, useOutletContext } from "react-router";
 
 function Navbar() {
+  const { username } = useOutletContext();
+
   return (
     <nav>
       <Link to="/">
         <h1>Ckylogs</h1>
       </Link>
+      <nav>
+        <ul>
+          <li>
+            {username && <div>Welcome {username}</div>}
+            {username ? <Logout></Logout> : <Link to="/login">Login</Link>}
+          </li>
+        </ul>
+      </nav>
     </nav>
   );
 }
