b5866d705789ed203789accd9ff8407dff2fa94e
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-21 14:09:30 +1100

    Add styling to post summary cards on home feed

--- frontend/public/src/components/PostSummary.jsx
+++ frontend/public/src/components/PostSummary.jsx
@@ -5,11 +5,13 @@ import { Link } from "react-router";
 function PostSummary({ id, title, author, createdOn, body }) {
   return (
     <div className={styles.card}>
+      <div className={styles.details}>
+        <Link to={`/user/${author}`}>{author}</Link>
+        <Timestamp dateTime={createdOn} />
+      </div>
       <Link to={`/post/${id}`}>
         <h2>{title}</h2>
       </Link>
-      <Link to={`/user/${author}`}>{author}</Link>
-      <Timestamp dateTime={createdOn} />
       <Link to={`/post/${id}`}>
         <p>{body}</p>
       </Link>
