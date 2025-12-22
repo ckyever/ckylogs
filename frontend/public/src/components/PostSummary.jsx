16d350496f9dfe5f6f0ee2b3acecaacab1d989df
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-22 20:29:05 +1100

    Align timestamp with title of post summary when author is not displayed

--- frontend/public/src/components/PostSummary.jsx
+++ frontend/public/src/components/PostSummary.jsx
@@ -7,13 +7,18 @@ function PostSummary({ id, title, author, createdOn, body }) {
     <div className={styles.container}>
       <Link to={`/post/${id}`} className={styles.postLink} />
       <div className={styles.card}>
-        <div className={styles.details}>
-          <Link to={`/user/${author}`} className={styles.authorLink}>
-            {author}
-          </Link>
-          <Timestamp dateTime={createdOn} />
+        {author && (
+          <div className={styles.details}>
+            <Link to={`/user/${author}`} className={styles.authorLink}>
+              {author}
+            </Link>
+            <Timestamp dateTime={createdOn} />
+          </div>
+        )}
+        <div className={styles.title}>
+          <h2>{title}</h2>
+          {!author && <Timestamp dateTime={createdOn} />}
         </div>
-        <h2>{title}</h2>
         <p>{body}</p>
       </div>
     </div>
