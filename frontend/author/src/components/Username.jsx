9c6f669341f1bd19d326859eb16877630137070c
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-23 22:10:02 +1100

    Remove broken links to user profiles from author frontend

--- frontend/author/src/components/Username.jsx
+++ frontend/author/src/components/Username.jsx
@@ -1,19 +1,17 @@
 import styles from "../styles/Username.module.css";
-import { Link, useOutletContext } from "react-router";
+import { useOutletContext } from "react-router";
 
 function Username({ username }) {
   const { username: currentUsername } = useOutletContext();
   const isCurrentUser = currentUsername === username;
   return (
-    <Link to={`/user/${username}`}>
-      <span
-        className={`${styles.username} ${
-          isCurrentUser ? styles.currentUser : null
-        }`}
-      >
-        {username}
-      </span>
-    </Link>
+    <span
+      className={`${styles.username} ${
+        isCurrentUser ? styles.currentUser : null
+      }`}
+    >
+      {username}
+    </span>
   );
 }
 
